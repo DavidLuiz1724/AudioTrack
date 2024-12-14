6fa2cd667b440091e0a9b7c051944533ec97bda9
root <davidluis1724@gmail.com>
2024-12-14 07:49:51 -0800

    fix update

--- server/apis/serializers.py
+++ server/apis/serializers.py
@@ -10,7 +10,7 @@ class TokenObtainPairSerializer(JwtTokenObtainPairSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
 
 
 class PasswordSerializer(serializers.Serializer):
